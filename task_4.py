89b762a1bc633f98ca2b8cc81cc56dfc33ca2711
BugSpace <409478@niuitmo.ru>
2026-03-05 10:40:24 +0700

    add a solution for task b

--- task_4.py
+++ task_4.py
@@ -1,3 +1,5 @@
+import math
+
 def task_a():
     result = 0.0
     for i in range(1, 100):
@@ -5,4 +7,12 @@ def task_a():
             result += 1 / (i + j**2)
     return result
 
+def task_b():
+    result = 0.0
+    for i in range(1, 100):
+        for j in range(1, 60):
+            result += math.sin(i**3 + j**4)
+    return result
+
 print(task_a())
+print(task_b())
