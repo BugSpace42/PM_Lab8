eebf137e2440484765e92e5dcb91c1a94d46267a
BugSpace <409478@niuitmo.ru>
2026-03-05 10:36:26 +0700

    add a function for finding gcd of multiple numbers

--- task_3.py
+++ task_3.py
@@ -1,11 +1,19 @@
 def gcd_two_numbers(a, b):
-    # Вычисляет НОД двух чисел с помощью алгоритма Евклида.]
+    # Вычисляет НОД двух чисел с помощью алгоритма Евклида.
     while b != 0:
         a, b = b, a % b
     return a
+
+
+def gcd_multiple_numbers(numbers):
+    result = gcd_two_numbers(numbers[0], numbers[1])
+    for i in range(2, len(numbers)):
+        result = gcd_two_numbers(result, numbers[i])
+    return result
 
 m = int(input("Введите m: "))
 n = []
 for i in range(1, m + 1):
     n.append(int(input(f"Введите m_{i}: ")))
-print(n)
+result = gcd_multiple_numbers(n)
+print(f"НОД введённых чисел равен {result}")
