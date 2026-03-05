9a47f2a8d80ac3501203e470c15010dd4059913d
BugSpace <409478@niuitmo.ru>
2026-03-05 10:24:00 +0700

    add function for finding all triples

--- task_2.py
+++ task_2.py
@@ -1,12 +1,18 @@
-def find_triple(n):
-    # Находит одну тройку (x, y, z) натуральных чисел таких, что x^2 + y^2 + z^2 = n.
+def find_triples(n):
+    # Находит все тройки (x, y, z) натуральных чисел таких, что x^2 + y^2 + z^2 = n.
+    triples = []
     for x in range(1, n):
         for y in range(x, n):
             for z in range(y, n):
                 remainder = n - x * x - y * y - z * z
                 if remainder == 0:
-                    return (x, y, z)
-    return None
+                    triples.append((x, y, z))
+    return triples
 
 n = int(input("Введите n: "))
-print(find_triple(n))
+triples = find_triples(n)
+if triples:
+    print(f"Есть {len(triples)} троек натуральных чисел таких, что x^2 + y^2 + z^2 = n:")
+    print(*triples, sep="\n")
+else:
+    print("Таких троек нет.")
