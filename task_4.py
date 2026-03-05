5d2cb9bbd42006187a5acbc2f41bf87770ffab70
BugSpace <409478@niuitmo.ru>
2026-03-05 10:43:13 +0700

    fix all solutions

--- task_4.py
+++ task_4.py
@@ -2,29 +2,29 @@ import math
 
 def task_a():
     result = 0.0
-    for i in range(1, 100):
-        for j in range(1, 50):
+    for i in range(1, 101):
+        for j in range(1, 51):
             result += 1 / (i + j**2)
     return result
 
 def task_b():
     result = 0.0
-    for i in range(1, 100):
-        for j in range(1, 60):
+    for i in range(1, 101):
+        for j in range(1, 61):
             result += math.sin(i**3 + j**4)
     return result
 
 def task_c():
     result = 0.0
-    for i in range(1, 100):
-        for j in range(1, 100):
+    for i in range(1, 101):
+        for j in range(1, 101):
             result += (j - i + 1) / (i + j)
     return result
 
 def task_d():
     result = 0.0
-    for i in range(1, 100):
-        for j in range(1, i):
+    for i in range(1, 101):
+        for j in range(1, i+1):
             result += 1 / (2 * j + i)
     return result
 
